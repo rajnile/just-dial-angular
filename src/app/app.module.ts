9e36f096c6ee130e72829052d1507fe08e335a86
raj nile <rajkumar1212.nile@gmail.com>
2022-05-17 22:40:24 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { FootMenuComponent } from './foot-menu/foot-menu.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ItemComponent } from './item/item.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    FootMenuComponent,
+    MainContainerComponent,
+    ListItemsComponent,
+    ItemComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/element-config.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {list-items} from './list.items.component';
+@Injectable({
+  providedIn: 'root'
+})
+export class ElementConfigService {
+
+  constructor() { }
+}
